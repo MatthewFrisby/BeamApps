ba9cd184620711cb410bea4a7e1006d16c028f9f
Matthew Frisby <matt21@live.unc.edu>
2019-02-26 15:44:41 -0500

    type

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({credentials: true,  origin: 'http://localhost:4200'}));
 
 // serve static files from template
-//app.use(express.static(__dirname + '/template'));
+app.use(express.static(__dirname + '/template'));
 
 // include routes
 var routes = require('./routes/router');
